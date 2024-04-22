acffb8044b7c32334feb091906f4ec613e967c8d
snowye7 <601994226@qq.com>
2024-04-22 14:40:33 +0800

    新增设置npm镜像

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 #!/usr/bin/env node
 import { getDescription, handlePrettier } from "./utils/index"
 import { Command } from "commander"
-import { input } from "@inquirer/prompts"
+import { input, select } from "@inquirer/prompts"
+import chalk from "chalk"
+import { exec } from "child_process"
 
 const name = "☃️  Snowye ❄️ "
+
+const theme = {
+    icon: {
+        cursor: "❄️ "
+    }
+}
 
 const program = new Command()
 
@@ -15,6 +23,31 @@ program
     .action(async () => {
         const result = await input({ message: "选择过滤的文件夹 空格隔开" })
         handlePrettier(result.split(" "))
+    })
+
+program
+    .command("npm")
+    .description(getDescription("设置npm源"))
+    .action(async () => {
+        exec("npm get registry", async (error, stdout) => {
+            if (error) {
+                console.error(`执行出错: ${error}`)
+                return
+            }
+            console.log(" 🐻 " + chalk.yellowBright("当前npm镜像源:" + stdout))
+            const result = await select({
+                message: "选择npm镜像源",
+                theme,
+                choices: [
+                    { name: "默认", value: "https://registry.npmjs.org/" },
+                    { name: "淘宝", value: "https://registry.npmmirror.com" },
+                    { name: "阿里云", value: "https://npm.aliyun.com" },
+                    { name: "腾讯云", value: "http://mirrors.cloud.tencent.com/npm/" },
+                    { name: "华为云", value: "https://mirrors.huaweicloud.com/repository/npm/" }
+                ]
+            })
+            exec(`npm config set registry ${result}`)
+        })
     })
 
 program.parse()
